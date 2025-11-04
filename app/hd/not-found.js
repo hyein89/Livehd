6e1aade75e07cfd700100d26bdfb076bdbd5b565
hyein89 <bihontai@gmail.com>
2025-11-04 18:12:14 +0700

    Create not-found.js

---
+++ app/hd/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main style={{ padding: "100px 20px" }}>
+      <h1>404 - Pertandingan Tidak Ditemukan</h1>
+      <p>Periksa kembali URL yang kamu masukkan.</p>
+    </main>
+  );
+}
