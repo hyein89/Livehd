becdaaeb0d8be0e8ddaf83117a00b1c3181ed46d
hyein89 <bihontai@gmail.com>
2025-11-04 18:10:28 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "100px 20px" }}>
+      <h1>Selamat Datang di Live Sports HD</h1>
+      <p>Silakan buka <code>/hd/slug</code> untuk melihat pertandingan.</p>
+    </main>
+  );
+}
